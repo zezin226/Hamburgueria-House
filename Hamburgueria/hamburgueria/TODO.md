168fe042b7f734a0337aac9847087342ef520a59
zezin226 <josearthuraraujolopes@gmail.com>
2026-05-27 11:06:40 -0300

    Add files via upload

---
+++ Hamburgueria/hamburgueria/TODO.md
@@ -0,0 +1,9 @@
+# TODO - Adicionar imagens nos cards
+
+- [x] Atualizar `js/dados.js` adicionando campo `imagem` em cada item do cardápio.
+- [x] Atualizar `js/app.js` para renderizar `<img>` dentro de cada card usando o campo `imagem`.
+- [x] Atualizar `css/style.css` com estilos para `.card-imagem` garantindo layout consistente.
+- [ ] Testar no navegador se as imagens carregam e se o layout permanece responsivo.
+
+
+
---
+++ Hamburgueria/hamburgueria/js/dados.js
@@ -0,0 +1,61 @@
+// Dados do cardápio/serviços do cliente.
+// Edite este arquivo para alterar os itens exibidos no site.
+const ITENS = [
+  {
+    nome: "King bacon",
+    descricao:
+      "Chegou o novo King Bacon! Burger de carne 100% bovina grelhada no fogo, uma generosa porção de 6 fatias de bacon, 2 camadas da nossa irresistível Baconese e 2 fatias de queijo sabor cheddar em um pão brioche. A escolha perfeita para os amantes de bacon!",
+    preco: "R$ 15,00",
+    imagem: "img/img4.jpg",
+  },
+  {
+    nome: "King Duplo bacon",
+    descricao:
+      "Chegou o novo King Duplo Bacon! 2 carnes 100% bovinas grelhadas no fogo, uma generosa porção de 6 fatias de bacon, 3 camadas da nossa irresistível Baconese e 4 fatias de queijo sabor cheddar em um pão brioche. A escolha perfeita para os amantes de bacon!",
+    preco: "R$ 15,00",
+    imagem: "img/img5.jpg",
+  },
+  {
+    nome: "Mega Stacker Rodeio 2.0",
+    descricao:
+      "Duas camadas do tradicional Mega Stacker e a novidade do molho barbecue com seis onion rings! Esse é o Mega Stacker Rodeio 2.0. Vai encarar? Imagem meramente ilustrativa.",
+    preco: "R$ 15,00",
+    imagem: "img/img3.jpg",
+  },
+  {
+    nome: "Mega Stacker Rodeio 3.0",
+    descricao:
+      "Todo mundo ama a Família Mega Stacker e agora vai amar ainda mais! Além de três carnes, bacon, molho stacker e queijo derretido, o Rodeio leva o nosso delicioso molho barbecue e seis onion rings. Vai encarar? Imagem meramente ilustrativa.",
+    preco: "R$ 15,00",
+    imagem: "img/img1.jpg",
+  },
+  {
+    nome: "Whopper Rodeio",
+    descricao:
+      "Pão com gergelim, um suculento hambúrguer de pura carne bovina, cheddar fatiado, molho barbecue, 6 deliciosas onion rings, tomate, alface e maionese.",
+    preco: "R$ 20,00",
+    imagem: "img/img7.jpg",
+  },
+  {
+    nome: "Whopper Barbecue Bacon",
+    descricao:
+      "Adicionamos no nosso WHOPPER® um delicioso molho barbecue e fatias super crocantes de bacon. Resultado: Perfeição!",
+    preco: "R$ 20,00",
+    imagem: "img/img6.jpg",
+  },
+  {
+    nome: "Whopper Duplo",
+    descricao:
+      "Adicionamos no nosso WHOPPER® um delicioso molho barbecue e fatias super crocantes de bacon. Resultado: Perfeição!",
+    preco: "R$ 35,00",
+    imagem: "img/img8.jpg",
+  },
+  {
+    nome: "Whopper Furioso",
+    descricao:
+      "Pão com gergelim, um suculento hambúrguer de pura carne bovina, cheddar fatiado, bacon, alface, tomate, maionese, pimenta-jalapeño, onion rings e um delicioso molho furioso à base de mostarda e pimenta.",
+    preco: "R$ 20,00",
+    imagem: "img/img2.jpg",
+  },
+];
+
